ee706d53e09a5ba2b5fff8a8d113d41b23cd1854
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2026-02-19 05:39:22 +0530

    added navbar and home page

---
+++ medibot-frontend/app/page.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar/Navbar";
+import Home from "@/sections/Home";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+  <div>
+    <Navbar />
+    <Home />
+  </div>
+  );
+}
