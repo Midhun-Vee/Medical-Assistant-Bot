96a1aa95224ed037f00fa42b77edff89bcb89ec6
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2026-03-11 06:08:04 +0530

    added footer

---
+++ medibot-frontend/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-4 text-center text-sm text-black/60">
+      MediAssist @ 2026
+    </footer>
+  );
+};
+
+export default Footer;
